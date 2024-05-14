26e27ecda80a0c198f4956c3ab0196ba77205efa
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-05-14 13:05:36 +0100

    feature:new sidebar links

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -16,7 +16,7 @@ function SideBar() {
         <Menu color="white" size={40} />
       </SheetTrigger>
       <SheetContent>
-        <div className="h-full justify-center items-center flex-col flex text-xl gap-8">
+        <div className="h-full max-h-max justify-center items-center flex-col flex text-lg gap-4">
           <SheetClose asChild>
             <Link
               spy={true}
@@ -35,6 +35,38 @@ function SideBar() {
               className="text-black dark:text-white hover:underline capitalize decoration-2 underline-offset-4 cursor-pointer"
             >
               buy
+            </a>
+          </SheetClose>
+          <SheetClose asChild>
+            <a
+              href="https://bscscan.com/token/0x48a510A3394C2A07506d10910EBEFf3E25b7a3f1"
+              className="text-black dark:text-white hover:underline capitalize decoration-2 underline-offset-4 cursor-pointer"
+            >
+              BSCSCAN
+            </a>
+          </SheetClose>
+          <SheetClose asChild>
+            <a
+              href="https://dexscreener.com/bsc/0xdc98307571709e048f8c6d1ff0bb48eab054e535"
+              className="text-black dark:text-white hover:underline capitalize decoration-2 underline-offset-4 cursor-pointer"
+            >
+              DEXSCREENER
+            </a>
+          </SheetClose>
+          <SheetClose asChild>
+            <a
+              href="https://www.geckoterminal.com/bsc/pools/0xdc98307571709e048f8c6d1ff0bb48eab054e535"
+              className="text-black dark:text-white hover:underline capitalize decoration-2 underline-offset-4 cursor-pointer"
+            >
+              GECKOTERMINAL
+            </a>
+          </SheetClose>
+          <SheetClose asChild>
+            <a
+              href="https://bscscan.com/tx/0x073ad776ef0796e7767b3118f2fe70caf29c2a06ce852d0e2c24b4fef195ed76"
+              className="text-black dark:text-white hover:underline capitalize decoration-2 underline-offset-4 cursor-pointer"
+            >
+              LP 100% Burned
             </a>
           </SheetClose>
           <SheetClose asChild>
